33a65b392751537b443663bba8fa5a022a351541
monalisali <monalisali@hotmail.com>
2020-11-04 23:03:00 +0800

    'update'

--- src/main/java/Dto/JingDongRequestDataDto.java
+++ src/main/java/Dto/JingDongRequestDataDto.java
@@ -4,11 +4,15 @@ public class JingDongRequestDataDto {
     private String keywordType;
     private String searchType;
     private String key;
+    private int categoryId;
+    private int cat2Id;
+    private int cat3Id;
 
-    public JingDongRequestDataDto(String keywordType, String searchType,String key) {
+    public JingDongRequestDataDto(String keywordType, String searchType,String key,int categoryId) {
         this.keywordType = keywordType;
         this.searchType = searchType;
         this.key = key;
+        this.categoryId = categoryId;
     }
 
     public String getSearchType() {
@@ -22,6 +26,9 @@ public class JingDongRequestDataDto {
     public String getKey() {
         return key;
     }
+
+    public int getCategoryId() { return categoryId; }
+
 
 
 }
--- src/main/java/modules/QuestionFromZhihu_1.java
+++ src/main/java/modules/QuestionFromZhihu_1.java
@@ -115,18 +115,20 @@ public class QuestionFromZhihu_1 {
             conn.setRequestMethod("POST");
             conn.setDoOutput(true);
             conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+            conn.setRequestProperty("Origin","https://union.jd.com");
             conn.setRequestProperty("Accept","application/json, text/plain, */*");
             conn.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36");
+            conn.setRequestProperty("Cookie", "__jdu=386261381; areaId=2; ipLoc-djd=2-78-51978-0; unpl=V2_ZzNtbUsFQxcmWhJdKRoJUGIEQlgRVUARIA5HACgaVANlVBNVclRCFnQURlRnGFkUZAMZXkNcQBxFCEdkeB5fA2AFEFlBZxBFLV0CFi9JH1c%2bbRVVS1VKEXULTlB8KWwGZzMSXHJVShJ2D0ZWchlsNWAzIm1CX0YQcA52VUsYbEczXxRVQVZEFjgKT1N4HlwHbgMiXHJU; __jdv=76161171|baidu-search|t_262767352_baidusearch|cpc|69838512956_0_8c13bce8c2dd46a4b325d70eb2973f09|1604496028968; shshshfpa=ffc13228-0aca-c07d-fbc1-5458d531853c-1604496031; shshshfp=ddf2c2623a0be24ef08dfdb8b2b93b7e; shshshfpb=wNvQ2qk%20BjJodhketiLDoOA%3D%3D; 3AB9D23F7A4B3C9B=MUAIMUXFJPYAJAC7JJEKOB6YD6ZEQGWKSO3PSPV777KXXNT6ZXY6VKIVINQJCGGVBDE2CPKHDQUX2S2UNSS7JBQ5YA; shshshsID=181a15efc9deeb48c6b941d07aa80f38_12_1604496493374; sidebarStatus=1; identity=47c02a20-f6d2-411c-f1af-edb3d5357c3d; ssid=\"X5UiXJJpRZu2GMCNRMV42A==\"; cid=NXJPMDkwNmdXNzE2N21LOTk2OGFJNzc2MW9SNDUyMmJUODUyM2JBMjc4NHJMMTky; thor=672F9D1F7DD935F9D21850F2696973E864EFB25192EF35B5BE462FF063B3511E95746E641E7FC99BBCF34F4589B8CC1DB66650B29C89FD1D749216B8DDF869C416F4D6D5003C40054EAA63C61420F35B3F212F3D5E19ABF28CBD87A565DBABF628F25CA954B6ACB1CBBC4D0F0FAF60FDE8AE3874A16E3905BF6405116C12ABA7EF29C2F9BDB79EC8F22510A3EF48D2A1; pinId=9dH6xvb346QJw0HQntXn2w; pin=monalisali360; unick=monalisali360; ceshi3.com=000; _tp=48j%2Fo%2F2en7PGkZCTn0apPw%3D%3D; logining=1; _pst=monalisali360; login=true; __jda=209449046.386261381.1604111950.1604111952.1604496029.2; __jdc=209449046; MMsgIdmonalisali360=615683; MNoticeIdmonalisali360=309; __jdb=209449046.56.386261381|2.1604496029; RT=\"z=1&dm=jd.com&si=7qif9hzgldi&ss=kh3fwt2r&sl=y&tt=18zm&ld=kr7h&nu=81457e7b6eec45a5e13d91d267f0378c&cl=k866\"");
             //conn.setRequestProperty("referer","https://union.jd.com/proManager/index?pageNo=1");
             conn.connect();
 
             DataOutputStream out = new DataOutputStream(conn.getOutputStream());
             JSONObject param = new JSONObject();
-            JingDongRequestDataDto d = new JingDongRequestDataDto("kt1","st1","电脑");
+            JingDongRequestDataDto d = new JingDongRequestDataDto("kt1","st1","",9987);
             param.put("data", d);
             param.put("pageNo", 1);
             param.put("pageSize",60);
-            param.put("searchUUID","");
+            param.put("searchUUID","3b05b51cb82046ba8540d73fbf07d91d");
             out.write(param.toString().getBytes("UTF-8"));
             out.flush();
             out.close();
